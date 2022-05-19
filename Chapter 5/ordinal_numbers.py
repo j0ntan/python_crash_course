9d156e0da8c69869dfca537a828992079c331736
Jonathan Gamboa <jgamboa0209@gmail.com>
2022-05-19 00:49:26 -0400

    Add Chapter 5 files

---
+++ Chapter 5/ordinal_numbers.py
@@ -0,0 +1,13 @@
+from random import random, randrange
+
+
+nums = list(range(1, 10))
+for num in nums:
+    if num == 1:
+        print("1st")
+    elif num == 2:
+        print("2nd")
+    elif num == 3:
+        print("3rd")
+    else:
+        print(f"{num}th")
