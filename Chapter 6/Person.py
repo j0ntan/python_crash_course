f2225f94cd0ee7d950d8344288b66a7fb218c023
Jonathan Gamboa <jgamboa0209@gmail.com>
2022-05-19 01:10:22 -0400

    Add Chapter 6 files

---
+++ Chapter 6/Person.py
@@ -0,0 +1,5 @@
+Sheila = {'first_name': 'Sheila', 'last_name': 'Gamboa',
+          'age': 30, 'city': 'New York'}
+print(f"First name is {Sheila['first_name']}")
+print(f"Last name is {Sheila['last_name']}")
+print(f"Age is {Sheila['age']}")
