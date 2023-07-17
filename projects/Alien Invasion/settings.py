5de68729db8532f7cfead2065564ba297769cbbc
unknown <Jonathan Gamboa>
2023-07-16 23:15:47 -0400

    Increase game difficulty on next level

--- projects/Alien Invasion/settings.py
+++ projects/Alien Invasion/settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    """A class to store all settings for Alien Invasion."""
+    """Initialize the game's static settings."""
 
     def __init__(self):
         """Initialize the game's settings."""
@@ -20,7 +20,24 @@ class Settings:
         self.bullets_allowed = 8
 
         # Alien settings
+        self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
         self.alien_speed = 0.4
-        self.fleet_drop_speed = 5
+
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
